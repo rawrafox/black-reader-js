dd64dbf3695dedad36290ac762f1f792693e8943
Aurora <aurora@aventine.se>
2019-01-11 23:42:40 +0100

    Whitespace

--- runner.js
+++ runner.js
@@ -4,13 +4,24 @@ require = require("esm")(module)
 
 let black = require("./black.js")
 let fs = require("fs")
+let path = require("path")
 
 let arguments = process.argv.slice(2)
 let context = new black.Context(new Map())
 
-for (let i = 0; i < arguments.length; i++) {
-  let buffer = fs.readFileSync(arguments[i])
-  let view = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength)
+function processPath(p) {
+  if (fs.statSync(p).isDirectory()) {
+    fs.readdirSync(p).forEach((file) => { processPath(path.join(p, file)) })
+  } else {
+    if (path.extname(p) == ".black") {
+      let buffer = fs.readFileSync(p)
+      let view = new DataView(buffer.buffer, buffer.byteOffset, buffer.byteLength)
 
-  console.log(black.read(view, context))
+      console.log(black.read(view, context))
+    }
+  }
+}
+
+for (let i = 0; i < arguments.length; i++) {
+  processPath(arguments[i])
 }
