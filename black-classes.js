83af400671254978d48c69edb8c1f1c355a1b0f9
cppctamber <pauloosterman@gmail.com>
2019-07-28 12:41:36 +1200

    Updated definitions - Added `aud.js` - Added `AudEventKey` - Added `AudEventCurve` - Added `aud.js` to `black-classes.js` - Updated `eve.js` - Added `EveAnimation` - Added `EveAnimationCommand` - Added `EveAnimationCurve` - Added `EveAnimationState` - Added `EveAnimationStateMachine` - Added `EveChildInheritProperties` - Updated `EveChildContainer` - Updated `tr2.js` - Removed `ParticleType` as it needs to result in a number not a string - Added `Tr2ActionPlaySound` - Added `Tr2ActionSpawnParticles` - Updated `Tr2ControllerFloatVariable` - Updated `Tr2PointLight` - Updated `Tr2ParticleElementDeclaration`

--- black-classes.js
+++ black-classes.js
@@ -4,10 +4,12 @@ import eve from './black-classes/eve.js'
 import sof from './black-classes/sof.js'
 import tr2 from './black-classes/tr2.js'
 import tri from './black-classes/tri.js'
+import aud from './black-classes/aud.js'
 
 eve(classes)
 sof(classes)
 tr2(classes)
 tri(classes)
+aud(classes)
 
 export default classes
